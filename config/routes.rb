a7e42c0d5cc9e0b398dc32b0ce48fcddba449297
Mustajab <mustajabzaheer51@gmail.com>
2022-10-04 12:06:53 +0500

    made the indentation

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,11 @@ Rails.application.routes.draw do
 
   root "post#index"
 
-
   devise_for :users
-
 devise_scope :user do
-   get '/users/sign_out' => 'devise/sessions#destroy'
-    resources :post
+  get '/users/sign_out' => 'devise/sessions#destroy'
 end
 
-
+  resources :post
 
 end
